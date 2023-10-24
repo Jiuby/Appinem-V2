ac5a2c12084bd90782c1c50fd9633a766812395f
Juan Velez <juane8w@gmai.com>
2023-10-23 22:46:56 -0500

    Proyecto Plantilla

--- appinem/urls.py
+++ appinem/urls.py
@@ -8,7 +8,11 @@ urlpatterns = [
     path('actualizaciones/', views.actualizaciones_view, name='actualizaciones'),
     path('noticias/', views.noticias_view, name='noticias'),
     path('login/', views.login_view, name='login'),
+    path('registro/', views.registro_view, name='registro'),
     path('login-admin/', views.login_admin_view, name='login_admin'),
     path('login-profesores/', views.login_profesores_view, name='login_profesores'),
+    path('appinemEstudiantes/', views.appinemEstudiantes_view, name='appinemEstudiantes'),
+    path('appinemProfesores/', views.appinemProfesores_view, name='appinemProfesores'),
+    path('appinemAdmin/', views.appinemAdmin_view, name='appinemAdmin'),
     # Otras URLs...
 ]
--- appinem_app/views.py
+++ appinem_app/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render, redirect
-from .forms import EstudianteForm, EstudianteLoginForm
-from .models import Estudiante
+from .forms import EstudianteForm
 from django.contrib.auth import authenticate, login
-
+from django.contrib import messages
 def index_view(request):
     return render(request, 'index.html')
 
@@ -14,34 +13,30 @@ def noticias_view(request):
 
 
 def login_view(request):
-    if request.method == 'POST':
-        # Si se está intentando iniciar sesión
-        login_form = EstudianteLoginForm(request, data=request.POST)
-        if login_form.is_valid():
-            tarjeta = login_form.cleaned_data['tarjeta']
-            clave = login_form.cleaned_data['clave']
-            user = authenticate(request, username=tarjeta, password=clave)
-            if user is not None:
-                login(request, user)
-                return redirect('index')
-    else:
-        login_form = EstudianteLoginForm()
+    return render(request, 'Login.html')
 
-    # Si no se está intentando iniciar sesión, se puede estar intentando registrar
+
+def registro_view(request):
     if request.method == 'POST':
-        registro_form = EstudianteForm(request.POST)
-        if registro_form.is_valid():
-            registro_form.save()
-            # Puedes redirigir a donde desees después del registro
+        form = EstudianteForm(request.POST)
+        if form.is_valid():
+            form.save()
             return redirect('index')
     else:
-        registro_form = EstudianteForm()
+        form = EstudianteForm()
 
-    return render(request, 'Login.html', {'login_form': login_form, 'registro_form': registro_form})
-
-
+    return render(request, "registro.html", {'form': form})
 def login_admin_view(request):
     return render(request, 'LoginAdmin.html')
 
 def login_profesores_view(request):
     return render(request, 'LoginProfesores.html')
+
+def appinemEstudiantes_view(request):
+    return render(request, 'appinemEstudiantes.html')
+
+def appinemProfesores_view(request):
+    return render(request, 'appinemProfesores.html')
+
+def appinemAdmin_view(request):
+    return render(request, 'appinemAdmin.html')
